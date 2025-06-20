51b8b591e323b8f145a984a1eaef6dcb749b10fc
Dufran <zibertua@gmail.com>
2025-06-20 20:56:08 +0300

    feat(hobby): added hobby section

--- components/hobbies/HobbiesList.tsx
+++ components/hobbies/HobbiesList.tsx
@@ -1,5 +1,26 @@
 import React from 'react'
+import { hobbies } from '../vars'
+
+import { List } from '@mantine/core'
 
 export default function HobbiesList() {
-  return <div>HobbiesList</div>
+  return (
+    <div>
+      <h2>My Hobbies:</h2>
+      <List>
+        {hobbies.map((hobby) => (
+          <List.Item
+            key={hobby.name}
+            icon={<hobby.icon />}
+            style={{ display: 'flex', alignItems: 'center' }}
+          >
+            {hobby.name}
+            <List>
+              <List.Item>{hobby.description}</List.Item>
+            </List>
+          </List.Item>
+        ))}
+      </List>
+    </div>
+  )
 }
